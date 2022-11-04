8ff48bfe7aa9fc4ecc902026344cf57641fe0070
tomusmac <82265152+tomusmac@users.noreply.github.com>
2022-11-04 13:04:28 +0100

    Update cw5.3.php

--- cw5.3.php
+++ cw5.3.php
@@ -7,14 +7,13 @@ function sortowanie($tablica) {
             $niepodzielne[]=$wartosc;
         }
 }
-rsort($podzielne);
+arsort($podzielne);
 asort($niepodzielne);
-return $podzielne+$niepodzielne;
+return array_merge($podzielne,$niepodzielne);
 }
-$tab = array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,5,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30);
+$tab = array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30);
 $tab=sortowanie($tab);
-foreach($tab as $wartosc){
-echo "$wartosc ";
+foreach($tab as $wartosc) {
+    echo "$wartosc <br>";
 }
-
 ?>
